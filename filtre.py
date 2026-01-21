cca53f53a95ba140f46c5778516b5d384fc8645d
Gerome Pecheur <gerome.pecheur@ign.fr>
2026-01-22 00:33:24 +0100

    gestion filtre, selection, table attributaire...

---
+++ filtre.py
@@ -0,0 +1,76 @@
+import os
+
+from PyQt5.QtCore import Qt, QObject
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from PyQt5.QtWidgets import QDialog, QListWidget
+from PyQt5.uic import loadUi
+
+
+class DialogFiltre(QObject):
+    def __init__(self,parent=None):
+        super().__init__()
+        self.parent = parent
+        self.dlg_filtre = None
+        self.model = None
+        self.all_is_checked = False
+
+    def ini_list_view(self):
+        # je check ou pas les colonnes en fct des colonnes masquées du dlg_liste (donc apes filtre)
+        entete_colonne = []
+        for col in range(self.parent.model.columnCount()):
+            if not self.parent.dlg_liste.tableView.isColumnHidden(col):
+                entete_colonne.append(self.parent.model.headerData(col, Qt.Horizontal))
+
+        layer,champs = self.parent.get_structure_layer()
+        self.model = QStandardItemModel()
+        for champ in champs:
+            item = QStandardItem(champ)
+            item.setCheckable(True)
+            item.setEditable(False)
+            if champ in entete_colonne:
+                item.setCheckState(Qt.Checked)
+            self.model.appendRow(item)
+        self.dlg_filtre.listView.setModel(self.model)
+
+    def sel_all_colonnes(self):
+        etat = Qt.Checked if not self.all_is_checked else Qt.Unchecked
+        # if etat == Qt.Checked:
+        #     self.dlg_filtre.pushButton_sel_all.setText("Rien")
+        # else:
+        #     self.dlg_filtre.pushButton_sel_all.setText("Tout")
+
+        for row in range(self.model.rowCount()):
+            item = self.model.item(row)
+            item.setCheckState(etat)
+
+        self.all_is_checked = not self.all_is_checked
+        self.dlg_filtre.pushButton_sel_all.setText("Rien" if self.all_is_checked else "Tout")
+
+    def get_checked_columns(self):
+        """Retourne la liste des colonnes cochées"""
+        list_checked = []
+        for row in range(self.model.rowCount()):
+            item = self.model.item(row)
+            if item.checkState() == Qt.Unchecked:
+                list_checked.append(item.text())
+        self.dlg_filtre.close()
+        return list_checked
+
+
+    def open_dialog(self,parent = None):
+        self.dlg_filtre = QDialog(parent)
+        loadUi(os.path.join(os.path.dirname(__file__), "filtre.ui"), self.dlg_filtre)
+        self.dlg_filtre.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowStaysOnTopHint)
+        self.dlg_filtre.setWindowTitle("Filtrer les colonnes visibles")
+
+        self.dlg_filtre.pushButton_sel_all.setText("Rien")
+
+        # slot
+        self.all_is_checked = True
+        self.dlg_filtre.pushButton_sel_all.clicked.connect(self.sel_all_colonnes)
+        self.dlg_filtre.pushButton_appliquer.clicked.connect(self.get_checked_columns)
+
+        self.ini_list_view()
+
+        result = self.dlg_filtre.exec_()
+
--- liste_dlg.py
+++ liste_dlg.py
@@ -1,10 +1,12 @@
 import os
 
-from PyQt5.QtCore import Qt, QObject
+from PyQt5.QtCore import Qt, QObject, QTimer
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import QDialog
 from PyQt5.uic import loadUi
 from qgis.core import QgsProject
+
+from .filtre import *
 
 class DialogListe(QObject):
     def __init__(self,plugin_parent=None):
@@ -16,17 +18,20 @@ class DialogListe(QObject):
         self.dialogs_liste = []
         self.parent = plugin_parent
 
-    def init_tableview(self,dlg):
+    def init_tableview(self):
         layers,champs = self.get_structure_layer()
-        champs = ["Layer"] + list(champs)
+        champs = ["Layer","id"] + list(champs)
         self.model = QStandardItemModel()
         self.model.setColumnCount(len(champs))
 
         self.model.setHorizontalHeaderLabels(champs)
-        dlg.tableView.setModel(self.model)
-        dlg.tableView.resizeColumnsToContents()
-        dlg.tableView.setColumnWidth(0, 100)
-        dlg.tableView.verticalHeader().setDefaultSectionSize(10)
+        self.dlg_liste.tableView.setModel(self.model)
+        self.dlg_liste.tableView.resizeColumnsToContents()
+        # colonne "layer" et "id"
+        self.dlg_liste.tableView.setColumnWidth(0, 100)
+        self.dlg_liste.tableView.setColumnWidth(1, 50)
+        self.dlg_liste.tableView.verticalHeader().setDefaultSectionSize(10)
+        self.dlg_liste.tableView.setSortingEnabled(True)
 
     def get_structure_layer(self):
         liste_champ = set()
@@ -46,10 +51,10 @@ class DialogListe(QObject):
         # vider le model avant insertion
         self.model.removeRows(0, self.model.rowCount())
 
-        # precision : le range commence à 1 et non à 0, car on a ajouté une colonne "layer" en premiere colonne
+        # precision : le range commence à  et non à 0, car on a ajouté une colonne "layer" et "id" en premiere colonne
         champs_entete = [
             self.model.headerData(col, Qt.Horizontal)
-            for col in range(1,self.model.columnCount())
+            for col in range(2,self.model.columnCount())
         ]
         # parcourir les entités
         for layer_name, ids_sel in self.dico_json.items():
@@ -66,6 +71,8 @@ class DialogListe(QObject):
                 row_items = []
                 item = QStandardItem(layer_name)
                 row_items.append(item)
+                item = QStandardItem(str(ident))
+                row_items.append(item)
                 for champ in champs_entete:
                     if feature.fields().indexOf(champ) != -1:
                         valeur = feature.attribute(champ)
@@ -78,16 +85,69 @@ class DialogListe(QObject):
 
             self.dlg_liste.label_nb_entite.setText(f"Nombre d'entités = {self.model.rowCount()}")
 
+    def open_table_attribut(self):
+        dico_layer_id = {}
+        selection_model = self.dlg_liste.tableView.selectionModel()
+        # récupérer les indexes sélectionnés
+        selected_indexes = selection_model.selectedRows()
+        for index in selected_indexes:
+            ligne = index.row()
+            # 1ere colonne des lignes sélectionnées --> "layer"
+            layer = self.model.item(ligne, 0).text()
+            # 2ieme colonne des lignes sélectionnées --> "id"
+            ident = self.model.item(ligne,1).text()
+            # si le layer n'est pas encore dans le dictionnaire, on crée une entrée vide
+            if layer not in dico_layer_id:
+                dico_layer_id[layer] = []
+
+            dico_layer_id[layer].append(ident)
+
+        # on commence par tout deselectionner
+        self.parent.deselectionne_all()
+
+        # pour chaque layer du dico on sélectionne les entites
+        # puis on ouvre la table attributaire de selection
+        project = QgsProject.instance()
+        layer = None
+        for layer_name,ident in dico_layer_id.items():
+            layers = project.mapLayersByName(layer_name)
+            layer = layers[0]
+            # transformation des id en entier
+            layer.selectByIds([int(fid) for fid in ident])
+            self.parent.iface.showAttributeTable(layer)
+
+        # IMPORTANT : le zomm ne se fait que sur les objet du dernier layer trouvé
+        self.parent.iface.setActiveLayer(layer)
+        self.parent.iface.actionZoomToSelected().trigger()
+
+    def hide_colonne(self):
+        # self.dlg_liste.tableView.hideColumn(1)
+        self.filtre = DialogFiltre(self)
+        self.filtre.open_dialog()
+
+        colonne_filtre = self.filtre.get_checked_columns()
+
+        # on masque les entêtes des colonnes qui sont dans le filtre
+        for col in range(self.model.columnCount()):
+            entete_colonne = self.model.headerData(col, Qt.Horizontal)
+            if entete_colonne in colonne_filtre:
+                self.dlg_liste.tableView.hideColumn(col)
+            else:
+                self.dlg_liste.tableView.showColumn(col)
+
     def open_liste(self):
         self.dlg_liste = QDialog()
         loadUi(os.path.join(os.path.dirname(__file__), "liste.ui"), self.dlg_liste)
         self.dlg_liste.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowStaysOnTopHint)
 
+        # slot
+        self.dlg_liste.pushButtonOpenTableAttribut.clicked.connect(self.open_table_attribut)
+        self.dlg_liste.pushButtonHide.clicked.connect(self.hide_colonne)
+
         # gestion d'ouverture de plusieurs dialog liste
         self.dialogs_liste.append(self.dlg_liste)
         self.dlg_liste.setAttribute(Qt.WA_DeleteOnClose)
-        self.dlg_liste.destroyed.connect(lambda: self.dialogs_liste.remove(self.dlg_liste))
-
+        self.dlg_liste.destroyed.connect(lambda _, dlg = self.dlg_liste:self.dialogs_liste.remove(dlg))
         self.dlg_liste.setWindowTitle(self.parent.get_nom_list_sel())
 
         # recuperation des données de la liste sélectionnée
@@ -95,7 +155,7 @@ class DialogListe(QObject):
         self.dico_json = self.parent.get_dico_from_json(nom_liste)
 
         # initialisation du tableview (creation des colonnes en fonction des champs des layers du json)
-        self.init_tableview(self.dlg_liste)
+        self.init_tableview()
 
         self.get_sel_in_list()
         self.dlg_liste.show()
